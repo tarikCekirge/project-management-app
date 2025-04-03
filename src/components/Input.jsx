d031e59c412aa7c160061b0ff70f9e2ffc50344f
Tarık Çekirge <tcekirge@gmail.com>
2025-04-03 19:39:52 +0300

    Styling Buttons & Inputs with Tailwind CSS

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Input = ({ textArea, name, label, ...props }) => {
+    const classes = "w-full p-1 border-b-2  border-slate-300 bg-slate-200 text-stone-800 focus:ring-0 outline-0 focus:border-slate-600"
     return (
-        <p>
-            <label htmlFor={name}>{label}</label>
-            {textArea ? <textarea name={name} {...props} /> : <input type="text" name={name} {...props} />}
+        <p className='flex flex-col gap-1 my-4'>
+            <label className='text-sm font-bold text-stone-400' htmlFor={name}>{label}</label>
+            {textArea ? <textarea className={classes} name={name} {...props} /> : <input type="text" name={name} {...props} className={classes} />}
         </p>
     )
 }
